171946a38ee47dcd285485833dc98c0da3edbcf2
Max Leiter <maxwell.leiter@gmail.com>
2022-03-06 19:20:38 -0800

    Remove loading for /new

--- client/pages/new.tsx
+++ client/pages/new.tsx
@@ -5,10 +5,14 @@ import { Loading, Page } from '@geist-ui/core'
 import useSignedIn from '../lib/hooks/use-signed-in'
 import Header from '../components/header'
 import { ThemeProps } from './_app'
+import { useRouter } from 'next/router'
 
 const Home = ({ theme, changeTheme }: ThemeProps) => {
+  const router = useRouter()
   const { isSignedIn, isLoading } = useSignedIn({ redirectIfNotAuthed: true })
-
+  if (!isSignedIn && !isLoading) {
+    router.push("/signin")
+  }
   return (
     <Page className={styles.container}>
       <Head>
@@ -22,7 +26,6 @@ const Home = ({ theme, changeTheme }: ThemeProps) => {
       </Page.Header>
 
       <Page.Content width={"var(--main-content-width)"} margin="0 auto" className={styles.main}>
-        {isLoading && <Loading />}
         {isSignedIn && <HomeComponent />}
       </Page.Content>
     </Page >
