ada427b2d7ac858fe24d7f079c0c4cf62b727dbc
Max Leiter <maxwell.leiter@gmail.com>
2022-03-08 02:43:04 -0800

    client: accessible emoji

--- client/components/preview/index.tsx
+++ client/components/preview/index.tsx
@@ -1,11 +1,12 @@
 import { memo } from "react"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
+import a11yEmoji from '@fec/remark-a11y-emoji';
 import styles from './preview.module.css'
 
 const MarkdownPreview = ({ content, height }: { content?: string, height?: number | string }) => {
     {/* remarkGfm is github flavored markdown support */ }
-    return (<div style={{ height }}><ReactMarkdown className={styles.markdownPreview} remarkPlugins={[remarkGfm]} >
+    return (<div style={{ height }}><ReactMarkdown className={styles.markdownPreview} remarkPlugins={[remarkGfm, a11yEmoji]} >
         {content || ""}
     </ReactMarkdown></div>)
 }
