fdf7e9e95185fd57ab88b5dfef2f5decf3f14cd3
lordvins226 <kevineilboudo54@gmail.com>
2022-03-15 19:34:33 +0000

    client: cleanup formatting button component

--- client/components/document/formatting-icons/index.tsx
+++ client/components/document/formatting-icons/index.tsx
@@ -1,137 +1,106 @@
-import { ButtonGroup, Button } from "@geist-ui/core"
-import { Bold, Italic, Link, Image as ImageIcon } from '@geist-ui/icons'
-import { RefObject, useCallback, useMemo } from "react"
-import styles from '../document.module.css'
+import { ButtonGroup, Button } from "@geist-ui/core";
+import { Bold, Italic, Link, Image as ImageIcon } from "@geist-ui/icons";
+import { RefObject, useCallback, useMemo } from "react";
+import styles from "../document.module.css";
+import useFormattingClick from "../hooks/use-formatting-click";
 
-// TODO: clean up
+const FormattingIcons = ({
+  textareaRef,
+  setText,
+}: {
+  textareaRef?: RefObject<HTMLTextAreaElement>;
+  setText?: (text: string) => void;
+}) => {
+  const { currentTextareaRef, currentSetText, onClick } = useFormattingClick({
+    currentTextareaRef: textareaRef,
+    currentSetText: setText,
+  });
 
-const FormattingIcons = ({ textareaRef, setText }: { textareaRef?: RefObject<HTMLTextAreaElement>, setText?: (text: string) => void }) => {
-    // const { textBefore, textAfter, selectedText } = useMemo(() => {
-    //     if (textareaRef && textareaRef.current) {
-    //         const textarea = textareaRef.current
-    //         const text = textareaRef.current.value
-    //         const selectionStart = textarea.selectionStart
-    //         const selectionEnd = textarea.selectionEnd
-    //         const textBefore = text.substring(0, selectionStart)
-    //         const textAfter = text.substring(selectionEnd)
-    //         const selectedText = text.substring(selectionStart, selectionEnd)
-    //         return { textBefore, textAfter, selectedText }
-    //     }
-    //     return { textBefore: '', textAfter: '' }
-    // }, [textareaRef,])
+  const handleBoldClick = useCallback(
+    (e) => {
+      if (currentTextareaRef?.current && currentSetText) {
+        onClick("bold")!;
+      }
+    },
+    [currentSetText, currentTextareaRef, onClick]
+  );
 
-    const handleBoldClick = useCallback((e) => {
-        if (textareaRef?.current && setText) {
-            const selectionStart = textareaRef.current.selectionStart
-            const selectionEnd = textareaRef.current.selectionEnd
-            const text = textareaRef.current.value
-            const before = text.substring(0, selectionStart)
-            const after = text.substring(selectionEnd)
-            const selectedText = text.substring(selectionStart, selectionEnd)
+  const handleItalicClick = useCallback(
+    (e) => {
+      if (currentTextareaRef?.current && currentSetText) {
+        onClick("italic")!;
+      }
+    },
+    [currentSetText, currentTextareaRef, onClick]
+  );
 
-            const newText = `${before}**${selectedText}**${after}`
-            setText(newText)
+  const handleLinkClick = useCallback(
+    (e) => {
+      if (currentTextareaRef?.current && currentSetText) {
+        onClick("link")!;
+      }
+    },
+    [currentSetText, currentTextareaRef, onClick]
+  );
 
-            // TODO; fails because settext async
-            textareaRef.current.setSelectionRange(before.length + 2, before.length + 2 + selectedText.length)
-        }
-    }, [setText, textareaRef])
+  const handleImageClick = useCallback(
+    (e) => {
+      if (currentTextareaRef?.current && currentSetText) {
+        onClick("image")!;
+      }
+    },
+    [currentSetText, currentTextareaRef, onClick]
+  );
 
-    const handleItalicClick = useCallback((e) => {
-        if (textareaRef?.current && setText) {
-            const selectionStart = textareaRef.current.selectionStart
-            const selectionEnd = textareaRef.current.selectionEnd
-            const text = textareaRef.current.value
-            const before = text.substring(0, selectionStart)
-            const after = text.substring(selectionEnd)
-            const selectedText = text.substring(selectionStart, selectionEnd)
-            const newText = `${before}*${selectedText}*${after}`
-            setText(newText)
-            textareaRef.current.focus()
-            textareaRef.current.setSelectionRange(before.length + 1, before.length + 1 + selectedText.length)
-        }
-    }, [setText, textareaRef])
+  const formattingActions = useMemo(
+    () => [
+      {
+        icon: <Bold />,
+        name: "bold",
+        action: handleBoldClick,
+      },
+      {
+        icon: <Italic />,
+        name: "italic",
+        action: handleItalicClick,
+      },
+      // {
+      //     icon: <Underline />,
+      //     name: 'underline',
+      //     action: handleUnderlineClick
+      // },
+      {
+        icon: <Link />,
+        name: "hyperlink",
+        action: handleLinkClick,
+      },
+      {
+        icon: <ImageIcon />,
+        name: "image",
+        action: handleImageClick,
+      },
+    ],
+    [handleBoldClick, handleImageClick, handleItalicClick, handleLinkClick]
+  );
 
-    const handleLinkClick = useCallback((e) => {
-        if (textareaRef?.current && setText) {
-            const selectionStart = textareaRef.current.selectionStart
-            const selectionEnd = textareaRef.current.selectionEnd
-            const text = textareaRef.current.value
-            const before = text.substring(0, selectionStart)
-            const after = text.substring(selectionEnd)
-            const selectedText = text.substring(selectionStart, selectionEnd)
-            let formattedText = '';
-            if (selectedText.includes('http')) {
-                formattedText = `[](${selectedText})`
-            } else {
-                formattedText = `[${selectedText}](https://)`
-            }
-            const newText = `${before}${formattedText}${after}`
-            setText(newText)
-            textareaRef.current.focus()
-            textareaRef.current.setSelectionRange(before.length + 1, before.length + 1 + selectedText.length)
-        }
-    }, [setText, textareaRef])
+  return (
+    <div className={styles.actionWrapper}>
+      <ButtonGroup className={styles.actions}>
+        {formattingActions.map(({ icon, name, action }) => (
+          <Button
+            auto
+            scale={2 / 3}
+            px={0.6}
+            aria-label={name}
+            key={name}
+            icon={icon}
+            onMouseDown={(e) => e.preventDefault()}
+            onClick={action}
+          />
+        ))}
+      </ButtonGroup>
+    </div>
+  );
+};
 
-    const handleImageClick = useCallback((e) => {
-        if (textareaRef?.current && setText) {
-            const selectionStart = textareaRef.current.selectionStart
-            const selectionEnd = textareaRef.current.selectionEnd
-            const text = textareaRef.current.value
-            const before = text.substring(0, selectionStart)
-            const after = text.substring(selectionEnd)
-            const selectedText = text.substring(selectionStart, selectionEnd)
-            let formattedText = '';
-            if (selectedText.includes('http')) {
-                formattedText = `![](${selectedText})`
-            } else {
-                formattedText = `![${selectedText}](https://)`
-            }
-            const newText = `${before}${formattedText}${after}`
-            setText(newText)
-            textareaRef.current.focus()
-            textareaRef.current.setSelectionRange(before.length + 1, before.length + 1 + selectedText.length)
-        }
-    }, [setText, textareaRef])
-
-    const formattingActions = useMemo(() => [
-        {
-            icon: <Bold />,
-            name: 'bold',
-            action: handleBoldClick
-        },
-        {
-            icon: <Italic />,
-            name: 'italic',
-            action: handleItalicClick
-        },
-        // {
-        //     icon: <Underline />,
-        //     name: 'underline',
-        //     action: handleUnderlineClick
-        // },
-        {
-            icon: <Link />,
-            name: 'hyperlink',
-            action: handleLinkClick
-        },
-        {
-            icon: <ImageIcon />,
-            name: 'image',
-            action: handleImageClick
-        }
-    ], [handleBoldClick, handleImageClick, handleItalicClick, handleLinkClick])
-
-    return (
-        <div className={styles.actionWrapper}>
-            <ButtonGroup className={styles.actions}>
-                {formattingActions.map(({ icon, name, action }) => (
-                    <Button auto scale={2 / 3} px={0.6} aria-label={name} key={name} icon={icon} onMouseDown={(e) => e.preventDefault()} onClick={action} />
-                ))}
-            </ButtonGroup>
-        </div>
-    )
-
-}
-
-export default FormattingIcons
+export default FormattingIcons;
---
+++ client/components/document/hooks/use-formatting-click.ts
@@ -0,0 +1,75 @@
+import type { RefObject } from "react";
+
+const useFormattingClick = ({
+  currentTextareaRef,
+  currentSetText,
+}: {
+  currentTextareaRef?: RefObject<HTMLTextAreaElement>;
+  currentSetText?: (text: string) => void;
+}) => {
+  const onClick = (type?: string) => {
+    if (currentTextareaRef?.current && currentSetText) {
+      const selectionStart = currentTextareaRef.current.selectionStart;
+      const selectionEnd = currentTextareaRef.current.selectionEnd;
+      const text = currentTextareaRef.current.value;
+      const before = text.substring(0, selectionStart);
+      const after = text.substring(selectionEnd);
+      const selectedText = text.substring(selectionStart, selectionEnd);
+
+      if (selectedText.length > 0) {
+        switch (type) {
+          case "bold": {
+            const newText = `${before}**${selectedText}**${after}`;
+            currentSetText(newText);
+            break;
+          }
+          case "italic": {
+            const newText = `${before}*${selectedText}*${after}`;
+            currentSetText(newText);
+            break;
+          }
+          case "link": {
+            let formattedText = "";
+            if (selectedText.includes("http")) {
+              formattedText = `[](${selectedText})`;
+            } else {
+              formattedText = `[${selectedText}](https://)`;
+            }
+            const newText = `${before}${formattedText}${after}`;
+            currentSetText(newText);
+            break;
+          }
+          case "image": {
+            let formattedText = "";
+            if (selectedText.includes("http")) {
+              formattedText = `![](${selectedText})`;
+            } else {
+              formattedText = `![${selectedText}](https://)`;
+            }
+            const newText = `${before}${formattedText}${after}`;
+            currentSetText(newText);
+            break;
+          }
+        }
+        if (type === "bold") {
+          currentTextareaRef.current.focus();
+          currentTextareaRef.current.setSelectionRange(
+            before.length + 2,
+            before.length + 2 + selectedText.length
+          );
+        }
+        if (type === "italic" || type === "link" || type === "image") {
+          currentTextareaRef.current.focus();
+          currentTextareaRef.current.setSelectionRange(
+            before.length + 1,
+            before.length + 1 + selectedText.length
+          );
+        }
+      }
+    }
+  };
+
+  return { currentTextareaRef, currentSetText, onClick };
+};
+
+export default useFormattingClick;
