0b2a4581fba7bd952312f5861bdcfa014976969d
lordvins226 <kevineilboudo54@gmail.com>
2022-03-16 16:21:18 +0000

    client: fix the ability to click without highlighting text

--- client/lib/hooks/use-formatting-click.ts
+++ client/lib/hooks/use-formatting-click.ts
@@ -16,55 +16,53 @@ const useFormattingClick = ({
       const after = text.substring(selectionEnd);
       const selectedText = text.substring(selectionStart, selectionEnd);
 
-      if (selectedText.length > 0) {
-        switch (type) {
-          case "bold": {
-            const newText = `${before}**${selectedText}**${after}`;
-            currentSetText(newText);
-            break;
-          }
-          case "italic": {
-            const newText = `${before}*${selectedText}*${after}`;
-            currentSetText(newText);
-            break;
-          }
-          case "link": {
-            let formattedText = "";
-            if (selectedText.includes("http")) {
-              formattedText = `[](${selectedText})`;
-            } else {
-              formattedText = `[${selectedText}](https://)`;
-            }
-            const newText = `${before}${formattedText}${after}`;
-            currentSetText(newText);
-            break;
-          }
-          case "image": {
-            let formattedText = "";
-            if (selectedText.includes("http")) {
-              formattedText = `![](${selectedText})`;
-            } else {
-              formattedText = `![${selectedText}](https://)`;
-            }
-            const newText = `${before}${formattedText}${after}`;
-            currentSetText(newText);
-            break;
-          }
+      switch (type) {
+        case "bold": {
+          const newText = `${before}**${selectedText}**${after}`;
+          currentSetText(newText);
+          break;
         }
-        if (type === "bold") {
-          currentTextareaRef.current.focus();
-          currentTextareaRef.current.setSelectionRange(
-            before.length + 2,
-            before.length + 2 + selectedText.length
-          );
+        case "italic": {
+          const newText = `${before}*${selectedText}*${after}`;
+          currentSetText(newText);
+          break;
         }
-        if (type === "italic" || type === "link" || type === "image") {
-          currentTextareaRef.current.focus();
-          currentTextareaRef.current.setSelectionRange(
-            before.length + 1,
-            before.length + 1 + selectedText.length
-          );
+        case "link": {
+          let formattedText = "";
+          if (selectedText.includes("http")) {
+            formattedText = `[](${selectedText})`;
+          } else {
+            formattedText = `[${selectedText}](https://)`;
+          }
+          const newText = `${before}${formattedText}${after}`;
+          currentSetText(newText);
+          break;
         }
+        case "image": {
+          let formattedText = "";
+          if (selectedText.includes("http")) {
+            formattedText = `![](${selectedText})`;
+          } else {
+            formattedText = `![${selectedText}](https://)`;
+          }
+          const newText = `${before}${formattedText}${after}`;
+          currentSetText(newText);
+          break;
+        }
+      }
+      if (type === "bold") {
+        currentTextareaRef.current.focus();
+        currentTextareaRef.current.setSelectionRange(
+          before.length + 2,
+          before.length + 2 + selectedText.length,
+        );
+      }
+      if (type === "italic" || type === "link" || type === "image") {
+        currentTextareaRef.current.focus();
+        currentTextareaRef.current.setSelectionRange(
+          before.length + 1,
+          before.length + 1 + selectedText.length,
+        );
       }
     }
   };
