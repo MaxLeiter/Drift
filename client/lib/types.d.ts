d4120e6f415864d8ca88be9e021ac47bee1a9e7d
Max Leiter <maxwell.leiter@gmail.com>
2022-03-23 15:34:23 -0700

    client: add prettier, switch to preact

--- client/lib/types.d.ts
+++ client/lib/types.d.ts
@@ -1,29 +1,29 @@
 export type PostVisibility = "unlisted" | "private" | "public" | "protected"
 
 export type ThemeProps = {
-    theme: "light" | "dark" | string,
-    changeTheme: () => void
+  theme: "light" | "dark" | string
+  changeTheme: () => void
 }
 
 export type Document = {
-    title: string
-    content: string
-    id: string
+  title: string
+  content: string
+  id: string
 }
 
 export type File = {
-    id: string
-    title: string
-    content: string
-    html: string
+  id: string
+  title: string
+  content: string
+  html: string
 }
 
 type Files = File[]
 
 export type Post = {
-    id: string
-    title: string
-    description: string
-    visibility: PostVisibility
-    files: Files
+  id: string
+  title: string
+  description: string
+  visibility: PostVisibility
+  files: Files
 }
--- client/pages/api/markdown/[id].ts
+++ client/pages/api/markdown/[id].ts
@@ -1,43 +1,54 @@
-import type { NextApiHandler } from "next";
+import type { NextApiHandler } from "next"
 
-import markdown from "@lib/render-markdown";
+import markdown from "@lib/render-markdown"
 
 const renderMarkdown: NextApiHandler = async (req, res) => {
-    const { id } = req.query
-    const file = await fetch(`${process.env.API_URL}/files/raw/${id}`, {
-        headers: {
-            'Accept': 'text/plain',
-            'x-secret-key': process.env.SECRET_KEY || '',
-            'Authorization': `Bearer ${req.cookies['drift-token']}`,
-        }
-    })
-
-    const json = await file.json()
-    const { content, title } = json
-    const renderAsMarkdown = ['markdown', 'md', 'mdown', 'mkdn', 'mkd', 'mdwn', 'mdtxt', 'mdtext', 'text', '']
-    const fileType = () => {
-        const pathParts = title.split(".")
-        const language = pathParts.length > 1 ? pathParts[pathParts.length - 1] : ""
-        return language
+  const { id } = req.query
+  const file = await fetch(`${process.env.API_URL}/files/raw/${id}`, {
+    headers: {
+      Accept: "text/plain",
+      "x-secret-key": process.env.SECRET_KEY || "",
+      Authorization: `Bearer ${req.cookies["drift-token"]}`
     }
-    const type = fileType()
-    let contentToRender: string = '\n' + content;
+  })
 
-    if (!renderAsMarkdown.includes(type)) {
-        contentToRender = `~~~${type}
+  const json = await file.json()
+  const { content, title } = json
+  const renderAsMarkdown = [
+    "markdown",
+    "md",
+    "mdown",
+    "mkdn",
+    "mkd",
+    "mdwn",
+    "mdtxt",
+    "mdtext",
+    "text",
+    ""
+  ]
+  const fileType = () => {
+    const pathParts = title.split(".")
+    const language = pathParts.length > 1 ? pathParts[pathParts.length - 1] : ""
+    return language
+  }
+  const type = fileType()
+  let contentToRender: string = "\n" + content
+
+  if (!renderAsMarkdown.includes(type)) {
+    contentToRender = `~~~${type}
 ${content}
 ~~~`
-    }
+  }
 
-    if (typeof contentToRender !== 'string') {
-        res.status(400).send('content must be a string')
-        return
-    }
+  if (typeof contentToRender !== "string") {
+    res.status(400).send("content must be a string")
+    return
+  }
 
-    res.setHeader('Content-Type', 'text/plain')
-    res.setHeader('Cache-Control', 'public, max-age=4800')
-    res.status(200).write(markdown(contentToRender))
-    res.end()
+  res.setHeader("Content-Type", "text/plain")
+  res.setHeader("Cache-Control", "public, max-age=4800")
+  res.status(200).write(markdown(contentToRender))
+  res.end()
 }
 
 export default renderMarkdown
