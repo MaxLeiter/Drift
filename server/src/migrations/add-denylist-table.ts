3200d7d92e815073f6c34cadfd2dc69a8754bfdb
spykelion <ndilionel6@gmail.com>
2022-03-30 09:04:54 +0100

    add updateAt field to JWTDenyLists table

---
+++ server/src/migrations/add-denylist-table.ts
@@ -0,0 +1,28 @@
+"use strict"
+import { DataTypes } from "sequelize"
+import type { Migration } from "../database"
+
+export const up: Migration = async ({ context: queryInterface }) =>
+	queryInterface.createTable("JWTDenyLists", {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true,
+			unique: true
+		},
+		token: {
+			type: DataTypes.STRING
+		},
+		createdAt: {
+			type: DataTypes.DATE
+		},
+		updatedAt: {
+			type: DataTypes.DATE
+		},
+		deletedAt: {
+			type: DataTypes.DATE
+		}
+	})
+
+export const down: Migration = async ({ context: queryInterface }) =>
+	queryInterface.dropTable("JWTDenyLists")
