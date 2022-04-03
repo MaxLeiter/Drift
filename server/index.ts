ec4a17f3bdcd55a54b3e25a8d973b6fd709eb9e2
Joaquin Florius <j@florius.com.ar>
2022-04-03 11:35:02 +0200

    Fix Use default development variable in the code

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,4 @@
 import * as dotenv from "dotenv"
-import { existsSync } from "fs";
-
-if (existsSync(".env.local")) {
-    console.info("using `.env.local` environment file")
-    dotenv.config({ path: '.env.local' })
-} else {
-    dotenv.config()
-}
+dotenv.config()
 
 import "./src/server"
