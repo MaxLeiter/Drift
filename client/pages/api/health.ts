f81999241f272abf87a989ba1440d86441bc831c
Max Leiter <max.leiter@vercel.com>
2022-11-14 01:30:09 -0800

    health check API route

---
+++ client/pages/api/health.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+const handler = async (_: NextApiRequest, res: NextApiResponse) => {
+    return res.json({
+        status: "UP"
+    })
+}
+
+export default handler
