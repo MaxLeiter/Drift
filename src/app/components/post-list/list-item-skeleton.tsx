e21d8966690fdeffc76d000a251d2cb7ab66dbe0
Max Leiter <max.leiter@vercel.com>
2023-02-21 23:48:34 -0800

    Improve list item skeleton on desktop

--- src/app/components/post-list/list-item-skeleton.tsx
+++ src/app/components/post-list/list-item-skeleton.tsx
@@ -5,7 +5,8 @@ import Skeleton from "@components/skeleton"
 export const ListItemSkeleton = () => (
 	<li>
 		<Card style={{ overflowY: "scroll" }}>
-			<div style={{ display: "flex", gap: 16 }}>
+			{/* TODO: this is a bad way to do skeletons and is onlya ccurate on desktop */}
+			<div style={{ display: "flex", gap: 16, marginBottom: 14 }}>
 				<div className={styles.title}>
 					{/* title */}
 					<Skeleton width={80} height={32} />
