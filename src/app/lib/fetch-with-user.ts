da6d576acb0215b388fcc7c67471481bf054d2b3
Max Leiter <max.leiter@vercel.com>
2023-07-16 20:48:06 -0700

    Fix getting host in fetch-with-user

--- src/app/lib/fetch-with-user.ts
+++ src/app/lib/fetch-with-user.ts
@@ -7,7 +7,8 @@ import { getSession } from "next-auth/react"
 export async function fetchWithUser(url: string, options: RequestInit = {}) {
 	// TODO: figure out if this extra network call hurts performance
 	const session = await getSession()
-	const newUrl = new URL(url, `${process.env.NEXT_PUBLIC_DRIFT_URL}`)
+	const host = document.location.host
+	const newUrl = new URL(url, `http://${host}`)
 	newUrl.searchParams.append("userId", session?.user.id || "")
 	return fetch(newUrl.toString(), options)
 }
